16cddec417c5b7d5aefe1bed2436c0a93cc39990
Nalinee04 <162853604+Nalinee04@users.noreply.github.com>
2024-09-02 23:38:01 +0700

    add singin,signup

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -14,7 +14,7 @@ import {
 
 const Header = () => {
   return (
-    <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6 z-[9999]">
+    <header className=" top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6 ">
       <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
         <Link
           href="/home"
@@ -102,10 +102,9 @@ const Header = () => {
           <DropdownMenuContent align="end">
             <DropdownMenuLabel>My Account</DropdownMenuLabel>
             <DropdownMenuSeparator />
-            <DropdownMenuItem>Settings</DropdownMenuItem>
-            <DropdownMenuItem>Support</DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Logout</DropdownMenuItem>
+            <DropdownMenuItem>Sign in</DropdownMenuItem>
+            <DropdownMenuItem>Sign up</DropdownMenuItem>
+            
           </DropdownMenuContent>
         </DropdownMenu>
       </div>
