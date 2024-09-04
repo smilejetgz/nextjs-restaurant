fdef121dd1402f9527a34b2d4c51aea043479f2c
Nalinee04 <162853604+Nalinee04@users.noreply.github.com>
2024-09-04 10:01:34 +0700

    Updated Header.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -35,6 +35,21 @@ const Header = () => {
           className="text-muted-foreground transition-colors hover:text-foreground"
         >
           Categories
+          <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="secondary" size="icon" className="rounded-full">
+              <CircleUser className="h-5 w-5" />
+              <span className="sr-only">Toggle user menu</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Categories</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem><Link href={'/tea'} >Sign in</Link></DropdownMenuItem>
+            <DropdownMenuItem><Link href={'/juice'} >Sign up</Link></DropdownMenuItem>
+            
+          </DropdownMenuContent>
+        </DropdownMenu>
         </Link>
       </nav>
       <Sheet>
